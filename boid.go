0adc0fbef28f31c77d835ff89d1b6ae4b3b0a3d5
rafidianto <rafidianto.code@gmail.com>
2021-06-02 16:52:59 +0700

    show the boids, make it move with considering acceleration based on average velocity of boids inside viewradius

---
+++ boid.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+type Boid struct {
+	position Vector2D
+	velocity Vector2D
+	id int
+}
+
+func (b *Boid) calcAcceleration() Vector2D{
+
+	upper, lower := b.position.AddV(viewRadius), b.position.AddV(-viewRadius)
+	avgVelocity := Vector2D{0,0}
+	count := 0.0
+
+	for i := math.Max(lower.x, 0); i <= math.Min(upper.x, screenWidth); i++ {
+		for j:= math.Max(lower.y, 0); j <= math.Min(upper.y, screenHeight); j++ {
+			if otherBoidId := boidMap[int(i)][int(j)]; otherBoidId != -1 && otherBoidId != b.id {
+				if dist := boids[otherBoidId].position.Distance(b.position); dist < viewRadius {
+					count++
+					avgVelocity = avgVelocity.Add(boids[otherBoidId].velocity)
+				}
+			}
+		}
+	}
+
+	accel := Vector2D{0,0}
+	if count > 0 {
+		avgVelocity = avgVelocity.DivisionV(count)
+		accel = avgVelocity.Substract(b.velocity).MultiplyV(adjRate)
+	}
+
+	return accel
+}
+
+func (b *Boid) moveOne() {
+	b.velocity = b.velocity.Add(b.calcAcceleration()).Limit(-1,1)
+	boidMap[int(b.position.x)][int(b.position.y)] = -1
+	b.position = b.position.Add(b.velocity)
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+
+	next := b.position.Add(b.velocity)
+
+	if next.x >= screenWidth || next.x < 0 {
+		b.velocity = Vector2D{x: -b.velocity.x, y: b.velocity.y}
+	}
+
+	if next.y >= screenHeight || next.y < 0 {
+		b.velocity = Vector2D{x: b.velocity.x, y: -b.velocity.y}
+	}
+}
+
+func (b *Boid) start() {
+	for {
+		b.moveOne()
+		time.Sleep(5 * time.Millisecond)
+	}
+}
+
+func createBoid(bid int) {
+	b := Boid{
+		position: Vector2D{x: rand.Float64() * screenWidth, y: rand.Float64() * screenHeight},
+		velocity: Vector2D{x: (rand.Float64() * 2) - 1.0, y: (rand.Float64() * 2) - 1.0},
+		id: bid,
+	}
+
+	boids[bid] = &b
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+	go b.start()
+}
